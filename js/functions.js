feb5da38f71eb707149986b7ad9240f0c3b5e253
mmazitov <mazitov.nikolya@gmail.com>
2022-12-22 18:49:38 +0200

    review

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 	initMobileMenu();
-	//initSlider();
+	initSlider();
 });
 function initMobileMenu() {
 	'use strict;';
@@ -23,11 +23,42 @@ function initMobileMenu() {
 function initSlider() {
 	'use strict;';
 	$('.cards__list').slick({
-		dots: true,
+		dots: false,
 		infinite: true,
+		arrows: true,
+		prevArrow: '.left__button',
+		nextArrow: '.right__button',
+		draggable: true,
 		speed: 300,
-		slidesToShow: 1,
-		centerMode: true,
-		variableWidth: true,
+		slidesToShow: 4,
+		slidesToScroll: 4,
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3,
+					infinite: true,
+					dots: true,
+				},
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+				},
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+				},
+			},
+			// You can unslick at a given breakpoint now by adding:
+			// settings: "unslick"
+			// instead of a settings object
+		],
 	});
 }
